d2e64eed48dad105ae3c2d14bbdd38cb005a6417
s_itoh.val00371 <s_itoh@val.co.jp>
2018-05-09 14:42:57 +0900

    S3へのデプロイスクリプトを追加しました。

--- 2018/bicycle_commute_calc/deploy.sh
+++ 2018/bicycle_commute_calc/deploy.sh
@@ -5,9 +5,9 @@ index.html
 app.js
 "
 
-for f in $FILES
+for f in ${FILES}
 do
-	echo "-=> Upload $f"
-	aws s3 cp app/$f s3://private.ekispert.com/tools/
+	echo "-=> Upload ${f}"
+	aws s3 cp app/${f} s3://${S3_TOOLS_BACKET}/
 done
 
